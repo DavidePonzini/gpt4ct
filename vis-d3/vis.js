929a8e74c479c9fec21620e198f2838d0ced0ee5
Davide Ponzini <davide.ponzini95@gmail.com>
2024-03-14 17:28:19 +0100

    exploration links

--- vis-d3/vis.js
+++ vis-d3/vis.js
@@ -35,7 +35,7 @@ function init_tree() {
 
 function update() {
     const svg_width = $('#tree').innerWidth();
-    const svg_heigth = $('#tree').innerHeight();
+    const svg_height = $('#tree').innerHeight();
 
     const margin = {
         left: 200,
@@ -47,7 +47,7 @@ function update() {
     const level_height = 200;
 
     const width = svg_width - margin.left - margin.right;
-    const heigth = svg_heigth - margin.top - margin.bottom;
+    const height = svg_height - margin.top - margin.bottom;
 
     const treeLayout = d3.tree();
     const treeData = treeLayout(d3.hierarchy(data, d => d.children));
@@ -61,12 +61,12 @@ function update() {
     // Nodes - Enter
     let nodesG_enter = nodes.enter().append('g')
         .classed('node', true)
-        .classed('node-internal', d => d.children)
-        .classed('node-leaf', d => !d.children)
-        .classed('node-unexplored', d => !d.subtasks && !d.solved)
-        .classed('node-explored', d => d.subtasks && !d.solved)
-        .classed('node-solved', d => d.solved)
-        .attr('transform', d => `translate(${d.x * width + margin.left}, ${d.y * heigth + margin.top})`)
+        .classed('node-internal', !isLeaf)
+        .classed('node-leaf', d => isLeaf)
+        .classed('unexplored', isUnexplored)
+        .classed('explored', isExplored)
+        .classed('solved', isSolved)
+        .attr('transform', d => `translate(${d.x * width + margin.left}, ${d.depth * level_height + margin.top})`)
         .on('click', onNodeClick);
     nodesG_enter.append('circle')
         .attr('r', 10);
@@ -77,13 +77,12 @@ function update() {
 
     // Nodes - Update
     let nodesG_update = nodes
-        .classed('node', true)
-        .classed('node-internal', d => d.children)
-        .classed('node-leaf', d => !d.children)
-        .classed('node-unexplored', d => !d.subtasks && !d.solved)
-        .classed('node-explored', d => d.subtasks && !d.solved)
-        .classed('node-solved', d => d.solved)
-        .attr('transform', d => `translate(${d.x * width + margin.left}, ${d.y * heigth + margin.top})`)
+        .classed('node-internal', !isLeaf)
+        .classed('node-leaf', d => isLeaf)
+        .classed('unexplored', isUnexplored)
+        .classed('explored', isExplored)
+        .classed('solved', isSolved)
+        .attr('transform', d => `translate(${d.x * width + margin.left}, ${d.depth * level_height + margin.top})`)
         .on('click', onNodeClick);
     nodesG_update.select('text')
         .text(d => d.data.name);
@@ -100,34 +99,64 @@ function update() {
     // Links - Enter
     let links_enter = links.enter().append('path')
         .classed('link', true)
+        .classed('link-internal', d => !isLeaf(d.target))
+        .classed('link-leaf', d => isLeaf(d.target))
+        .classed('unexplored', d => isUnexplored(d.target))
+        .classed('explored', d => isExplored(d.target))
+        .classed('solved', d => isSolved(d.target))
         .attr('d', d3.linkVertical()
         .source(d => [
             d.source.x * width + margin.left,
-            d.source.y * heigth + margin.top + 10.5
+            d.source.depth * level_height + margin.top + 10.5
         ])
         .target(d => [
             d.target.x * width + margin.left,
-            d.target.y * heigth + margin.top - 10.5   // 10 = circle radius; .5 = stroke width / 2
+            d.target.depth * level_height + margin.top - 10.5   // 10 = circle radius; .5 = stroke width / 2
         ])
         )
 
     // Links - Update
     let links_update = links;
     links_update
+        .classed('link-internal', d => !isLeaf(d.target))
+        .classed('link-leaf', d => isLeaf(d.target))
+        .classed('unexplored', d => isUnexplored(d.target))
+        .classed('explored', d => isExplored(d.target))
+        .classed('solved', d => isSolved(d.target))
         .attr('d', d3.linkVertical()
         .source(d => [
             d.source.x * width + margin.left,
-            d.source.y * heigth + margin.top + 10.5
+            d.source.depth * level_height + margin.top + 10.5
         ])
         .target(d => [
             d.target.x * width + margin.left,
-            d.target.y * heigth + margin.top - 10.5   // 10 = circle radius; .5 = stroke width / 2
+            d.target.depth * level_height + margin.top - 10.5   // 10 = circle radius; .5 = stroke width / 2
         ])
     )
 
     // Links - Exit
     links.exit().remove('path');
 
+}
+
+// Node has neither been explored (decomposed) or marked as solved
+function isUnexplored(node) {
+    return !node.data.subtasks && !node.data.solved;
+}
+
+// Node has been explored (decomposed) but not marked as solved
+function isExplored(node) {
+    return node.data.subtasks && !node.data.solved;
+}
+
+// Node has been marked as solved
+function isSolved(node) {
+    return node.data.solved;
+}
+
+// Node has no visible children
+function isLeaf(node) {
+    return !node.children;
 }
 
 function onNodeClick(event, item) {
