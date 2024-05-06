6fc7ff9a92ac2eb06883c3dffc3cc31af52a5d93
Davide Ponzini <davide.ponzini95@gmail.com>
2024-05-06 16:26:05 +0200

    seems to be working, but usage is empty

--- vis-d3/api/create_thread.php
+++ vis-d3/api/create_thread.php
@@ -1,6 +1,18 @@
 <?php
     include('threads.php');
 
-    print_r(create_thread());
+    $thread_id = create_thread();
 
+    $r = create_first_message($thread_id, 'Write a python program to download and store a webpage', 'Create the whole program');
+
+    print_r($r);
+    echo '<br>';
+    echo '<br>';
+
+    print_r($thread_id);
+    echo '<br>';
+    echo '<br>';
+
+    $r = run_thread($thread_id, 1);
+    print_r($r);
 ?>
--- vis-d3/api/threads.php
+++ vis-d3/api/threads.php
@@ -16,22 +16,22 @@
     }
 
     function create_message($thread_id, $role, $content) {
-        $result = make_request(
-            'POST',
-            'https://api.openai.com/v1/threads/' . $thread_id . '/messages',
+        $url = 'https://api.openai.com/v1/threads/' . $thread_id . '/messages';
+        
+        $result = make_request('POST', $url, json_encode(
             array(
                 'role' => $role,
                 'content' => $content
             )
-        );
+        ));
 
         return $result;
     }
 
-    function create_first_message($thread_id, $problem) {
+    function create_first_message($thread_id, $problem, $description) {
         $text = '-- Problem description --\n' . json_encode(array(
             'name' => $problem,
-            'description' => $problem
+            'description' => $description
         ));
     
         return create_message($thread_id, 'user', $text);
@@ -41,5 +41,23 @@
         $text = 'Using the same approach, decompose the task "' . $problem . '"';
   
         return create_message($thread_id, 'user', $text);
+    }
+
+    // todo: fare richiesta da curl e guardare il valore di usage
+    function run_thread($thread_id, $assistant_id) {
+        $url = 'https://api.openai.com/v1/threads/' . $thread_id . '/runs';
+
+        $result = make_request('POST', $url, json_encode(
+            array(
+                'assistant_id' => ASSISTANT_ID, // TO BE CHANGED
+                'response_format' => array('type' => 'json_object')
+            )
+        ));
+
+        $result = json_decode($result);
+        // $result = $result->result;
+
+        return $result;
+
     }
 ?>
