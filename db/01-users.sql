3245af4c6d3f13723c08b5d36f8b767c2c5e5f6e
Davide Ponzini <davide.ponzini95@gmail.com>
2024-06-10 10:41:15 +0200

    removed unnecessary files, added users

--- db/01-users.sql
+++ db/01-users.sql
@@ -2,7 +2,25 @@ BEGIN TRANSACTION;
 
 INSERT INTO problem_decomposition.users(user_id)
 VALUES
-    ('dav')
+    ('dav'),
+    ('dev'),
+    ('test'),
+    ('giovanna'),
+    ('phd1'),
+    ('phd2'),
+    ('phd3'),
+    ('phd4'),
+    ('phd5'),
+    ('phd6'),
+    ('phd7'),
+    ('phd8'),
+    ('phd9'),
+    ('phd10'),
+    ('phd11'),
+    ('phd12'),
+    ('phd13'),
+    ('phd14'),
+    ('phd15')
 ON CONFLICT (user_id) DO NOTHING;
 
 COMMIT;
